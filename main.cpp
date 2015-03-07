4febfb8637265b26952ced88c598a8728c25b728
Anthony Balocco <anthonybalocco@ymail.com>
2015-03-06 17:44:21 -0800

    Added main method
    Added the skeleton for the main method

---
+++ main.cpp
@@ -0,0 +1,40 @@
+//  
+//  Author: Alex Sanchez, Anthony Balocco
+//
+//  Class:  CS 570 - Operating Systems
+//  Prof:   Marie Roch  
+//  Date:   3/10/2015
+// 
+//  Filename: main.cpp
+//
+//  Description:
+//
+//     This is main method for the program.
+//
+//  Citations: Prof. Roch Lecture Notes and UNIX man pages
+//
+//  Copyright (c) 2015 Sanchez Balocco. All rights reserved.
+
+#include <iostream>
+
+#include "create_escargot.h"
+#include "create_frog.h"
+#include "lucy_and_ethel.h"
+
+using namespace std;
+
+int main (int argc, const char* argv[]) {
+
+    int escargot_delay;
+    int frog_delay;
+    int lucy_delay;
+    int ethel_delay;
+
+    // check command line args and set delays here
+
+    // start producing candies (conveyor belt starts)
+    // lucy/ethel pick up candies
+    // Loop until 100 candies produced
+
+    return 0;
+}
